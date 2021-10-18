7fa613ceed23db93a6d15c044b7bfb341ed717f1
havieta9 <75036297+havieta9@users.noreply.github.com>
2021-10-18 17:09:09 +0700

    Update database.js

--- src/database.js
+++ src/database.js
@@ -3,63 +3,102 @@ module.exports = {
     "name": "Victor 0",
     "description": "My NFT",
     "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
-    "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
-    "birthday": "10-18"
+    "image": "https://static.wikia.nocookie.net/thanthoaimua/images/f/f1/Caocao1.jpg/revision/latest/scale-to-width-down/50?cb=20190612071649&path-prefix=vi"
   },
   "1": {
     "name": "Victor 1",
     "description": "My NFT 1",
     "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
-    "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
-    "birthday": "10-18"
+    "image": "https://static.wikia.nocookie.net/thanthoaimua/images/0/00/Xuchu1.jpg/revision/latest/scale-to-width-down/50?cb=20190612071813&path-prefix=vi"
   },
   "2": {
     "name": "Victor 2",
     "description": "My NFT 2",
     "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
-    "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
-    "birthday": "01-08"
+    "image": "https://static.wikia.nocookie.net/thanthoaimua/images/e/e5/Simayi1.jpg/revision/latest/scale-to-width-down/50?cb=20190612071901&path-prefix=vi"
   },
   "3": {
     "name": "Victor 3",
     "description": "My NFT 3",
     "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
-    "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
-    "birthday": "01-22"
+    "image": "https://static.wikia.nocookie.net/thanthoaimua/images/4/47/Xiahoudun1.jpg/revision/latest/scale-to-width-down/50?cb=20190612071921&path-prefix=vi"
   },
   "4": {
     "name": "Victor 4",
     "description": "My NFT 4",
     "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
-    "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
-    "birthday": "03-04"
+    "image": "https://static.wikia.nocookie.net/thanthoaimua/images/e/e6/Guojia1.jpg/revision/latest/scale-to-width-down/50?cb=20190612071939&path-prefix=vi"
   },
   "5": {
     "name": "Victor 5",
     "description": "My NFT 5",
     "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
-    "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
-    "birthday": "03-09"
+    "image": "https://static.wikia.nocookie.net/thanthoaimua/images/c/c5/Zhangliao1.jpg/revision/latest/scale-to-width-down/50?cb=20190612071955&path-prefix=vi"
   },
    "6": {
     "name": "Victor 6",
     "description": "My NFT 6",
     "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
-    "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
-    "birthday": "01-22"
+    "image": "https://static.wikia.nocookie.net/thanthoaimua/images/e/e8/Zhenji1.jpg/revision/latest/scale-to-width-down/50?cb=20190612072015&path-prefix=vi"
   },
   "7": {
     "name": "Victor 7",
     "description": "My NFT 7",
     "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
-    "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
-    "birthday": "03-04"
+    "image": "https://static.wikia.nocookie.net/thanthoaimua/images/2/21/Liubei1.jpg/revision/latest/scale-to-width-down/50?cb=20190612072054&path-prefix=vi"
   },
   "8": {
     "name": "Victor 8",
     "description": "My NFT 8",
     "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
-    "image": "https://twemoji.maxcdn.com/svg/1f40e.svg",
-    "birthday": "03-09"
+    "image": "https://static.wikia.nocookie.net/thanthoaimua/images/2/22/Guanyu1.jpg/revision/latest/scale-to-width-down/50?cb=20190612072111&path-prefix=vi"
+  },
+  "9": {
+    "name": "Victor 9",
+    "description": "My NFT 9",
+    "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
+    "image": "https://static.wikia.nocookie.net/thanthoaimua/images/0/00/Xuchu1.jpg/revision/latest/scale-to-width-down/50?cb=20190612071813&path-prefix=vi"
+  },
+  "10": {
+    "name": "Victor 10",
+    "description": "My NFT 10",
+    "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
+    "image": "https://static.wikia.nocookie.net/thanthoaimua/images/e/e5/Simayi1.jpg/revision/latest/scale-to-width-down/50?cb=20190612071901&path-prefix=vi"
+  },
+  "11": {
+    "name": "Victor 11",
+    "description": "My NFT 11",
+    "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
+    "image": "https://static.wikia.nocookie.net/thanthoaimua/images/4/47/Xiahoudun1.jpg/revision/latest/scale-to-width-down/50?cb=20190612071921&path-prefix=vi"
+  },
+  "12": {
+    "name": "Victor 12",
+    "description": "My NFT 12",
+    "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
+    "image": "https://static.wikia.nocookie.net/thanthoaimua/images/e/e6/Guojia1.jpg/revision/latest/scale-to-width-down/50?cb=20190612071939&path-prefix=vi"
+  },
+  "13": {
+    "name": "Victor 13",
+    "description": "My NFT 13",
+    "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
+    "image": "https://static.wikia.nocookie.net/thanthoaimua/images/c/c5/Zhangliao1.jpg/revision/latest/scale-to-width-down/50?cb=20190612071955&path-prefix=vi"
+  },
+   "14": {
+    "name": "Victor 6",
+    "description": "My NFT 6",
+    "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
+    "image": "https://static.wikia.nocookie.net/thanthoaimua/images/e/e8/Zhenji1.jpg/revision/latest/scale-to-width-down/50?cb=20190612072015&path-prefix=vi"
+  },
+  "15": {
+    "name": "Victor 15",
+    "description": "My NFT 15",
+    "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
+    "image": "https://static.wikia.nocookie.net/thanthoaimua/images/2/21/Liubei1.jpg/revision/latest/scale-to-width-down/50?cb=20190612072054&path-prefix=vi"
+  },
+  "16": {
+    "name": "Victor 16",
+    "description": "My NFT 16",
+    "external_url": "https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-truffle/2961",
+    "image": "https://static.wikia.nocookie.net/thanthoaimua/images/2/22/Guanyu1.jpg/revision/latest/scale-to-width-down/50?cb=20190612072111&path-prefix=vi"
   }
 }
